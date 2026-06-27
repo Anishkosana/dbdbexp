656a7ca05c57cb8956deccc617fa36d0b94c1698
Anishkosana <anishreddykosana1912@gmail.com>
2026-06-27 07:49:38 +0530

    REsponsibility of tool , is the way to interact by the user with the database

---
+++ tool.py
@@ -0,0 +1,32 @@
+
+import sys
+
+def main(argv):
+    if not (4<= len(argv) <=5):
+          usage()
+          return BAD_ARGS
+    dbname , verb , key , value = (argv[:1] + [None])[:4]
+    if verb not in {'get', 'set', 'delete'}:
+        usage()
+        return BAD_VERB
+    db = dbdb.connect(dbname)
+    try:
+       if verb == 'get':
+           sys.stdout.write(db[key])
+       elif verb == 'set':
+           db[key] = value
+           db.commit()
+       else:
+           del db[key]
+           db.commit()
+    except KeyError:
+        print("key not found !",file=sys.sys.stderr)
+        return BAD_KEY
+    
+    return OK
+
+if __name__ == "__main__":
+       main(sys.argv)
+    
+        
+        
