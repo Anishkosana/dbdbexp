8687c760dcb38a470e738649fb5190cd3b7e191d
Anishkosana <anishreddykosana1912@gmail.com>
2026-09-18 11:47:06 +0530

    complete changes in file

--- tool.py
+++ tool.py
@@ -1,33 +1,29 @@
 
 import sys
-import dbdbexp 
-
+import dbdbexp
+def usage():
+    pass
 def main(argv):
-    if not (4<= len(argv) <=5):
+    if not (4 <= len(argv) <= 5):
+       Usage()
+       return BAD_ARGS
+    dbname, verb, key, value = (argv[1:] + [None])[:4]
+    if verb not in {'get','set','delete'}:
           usage()
-          return BAD_ARGS
-    dbname , verb , key , value = (argv[:1] + [None])[:4]
-    if verb not in {'get', 'set', 'delete'}:
-        usage()
-        return BAD_VERB
+          return BAD_VERB
     db = dbdbexp.connect(dbname)
     try:
-       if verb == 'get':
-           sys.stdout.write(db[key])
-       elif verb == 'set':
-           db[key] = value
-           db.commit()
-       else:
-           del db[key]
-           db.commit()
+        if verb == 'get':
+            sys.stdout.write(db[key])
+        elif verb == 'set':
+            db[key] = value
+            db.commit()
+        else:
+            del db[key]
+            db.commit()
     except KeyError:
-        print("key not found !",file=sys.sys.stderr)
+        print("key not found",file=sys.stderr)
         return BAD_KEY
-    
     return OK
-
-if __name__ == "__main__":
-       main(sys.argv)
     
-        
         
